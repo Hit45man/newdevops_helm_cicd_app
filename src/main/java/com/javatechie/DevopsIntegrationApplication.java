8f982c4ff52086c60b67b371449521d72d1ab1e2
Rohit Kamble <rohit_kamble@persistent.com>
2023-03-13 14:49:15 +0530

    updated the app file

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to Mr. DevOps Youtube Channel";
+		return "welcome to Welcome to Springboot App by Rohit Kamble";
 	}
 
 	public static void main(String[] args) {
